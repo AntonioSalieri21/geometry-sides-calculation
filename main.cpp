57d78a4b9e9c7de3f9202ae32fc3472167df90ff
AntonioSalieri21 <illia.antonov.st@vsb.cz>
2023-12-08 11:15:00 +0100

    Testing 3D sides calculations
    RESULTS ARE NEGATIVE
    Tried to use approach with registering all possible changes between individual columns, so we can break all surface into this prerecorded cases and calculate sides. In 2D it worked, but in 3D its has a lot of problems with calculating more sides that there exist.,
    Will try new algorithm, described in next updatre

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 #include <string>
 using namespace std;
 
-vector<string> readDataFromFile() 
+vector<string> readDataFromFile(string file) 
 {
     vector<string> res;
-    ifstream inputFile("data.txt");
+    ifstream inputFile(file);
     if (inputFile.is_open()) {
         string line;
         while (getline(inputFile, line)) {
@@ -55,13 +55,100 @@ int countSides2D(vector<string> data)
     return res;
 }
 
-int main() {
-    vector<string> data = readDataFromFile();
+int countSides3D(vector<string> data)
+{
+    int res = 6;
+    for(int y = 0; y < data.size(); y++) 
+    {
+        string line = data.at(y);
 
+        for(int x = 0; x < line.length(); x += 2)
+        {
+            int currentColumn = line.at(x) - '0';
+            /*int rightColumn = -1;
+            int leftColumn = -1;
+            int upColumn = -1;
+            int downColumn = -1;*/
+
+            vector<int> neighbours(4,-1); //clokwise - 0 - top, 1 - right, 2 - bottom, 3 - left
+
+            if(x + 2 < line.length()) neighbours.at(1) = line.at(x + 2) - '0';
+            if(x - 2 >= 0) neighbours.at(3) = line.at(x - 2) - '0';
+            if(y+1 < data.size()) 
+            {
+                string tmpLine = data.at(y+1);
+                if(x < tmpLine.length()) neighbours.at(2) = tmpLine.at(x) - '0';
+            } 
+            if(y-1 >= 0) 
+            {
+                string tmpLine = data.at(y-1);
+                if(x < tmpLine.length()) neighbours.at(0) = tmpLine.at(x) - '0';
+            } 
+            //cout << currentColumn << endl;
+            for(int neighbour : neighbours) 
+            {
+                //cout << neighbour << " ";
+                if(neighbour == -1) continue;
+                if(currentColumn == 0 || neighbour == 0) 
+                {
+                    res += 1;
+                    
+                    continue;
+                }
+
+                if(currentColumn != neighbour) 
+                {
+                    res += 2;
+                    continue;
+                }
+            }
+            //cout << endl;
+        }
+
+    }
+    return res;
+}
+
+int main() 
+{
+
+    vector<string> data = readDataFromFile("HeightMap1.txt");
+    cout << "HeightMap1.txt" << endl;
     for(string line : data) {
         cout << line << endl;
     }
-    int res = countSides2D(data);
+    int res = countSides3D(data);
     cout << "Number of sides: " << res << endl;
+    cout << endl;
+
+
+    data = readDataFromFile("HeightMap2.txt");
+    cout << "HeightMap2.txt" << endl;
+    for(string line : data) {
+        cout << line << endl;
+    }
+    res = countSides3D(data);
+    cout << "Number of sides: " << res << endl;
+    cout << endl;
+
+
+    data = readDataFromFile("HeightMap3.txt");
+    cout << "HeightMap3.txt" << endl;
+    for(string line : data) {
+        cout << line << endl;
+    }
+    res = countSides3D(data);
+    cout << "Number of sides: " << res << endl;
+    cout << endl;
+
+
+    data = readDataFromFile("HeightMap4.txt");
+    cout << "HeightMap4.txt" << endl;
+    for(string line : data) {
+        cout << line << endl;
+    }
+    res = countSides3D(data);
+    cout << "Number of sides: " << res << endl;
+
     return 0;
 }
