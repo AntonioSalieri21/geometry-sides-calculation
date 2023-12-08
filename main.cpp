f5fe7b8f85df32cb0d17e5fa07312d097df9cb82
AntonioSalieri21 <illia.antonov.st@vsb.cz>
2023-12-08 11:35:34 +0100

    New side counting test on 2D shapes
    Added new algorithm and tested it on 2D shape.
    Algorithm works by separating shape into vertical and horizontal sides, and counting them separately.
    Is more logical and stable. In next update would be implemented same algorithm in 3D space.

--- main.cpp
+++ main.cpp
@@ -22,97 +22,71 @@ vector<string> readDataFromFile(string file)
 
 int countSides2D(vector<string> data)
 {
-    int res = 4;
+    int resHor = 2; //left and right sides always exist
+    int resVer = 1; // bottom side always exists
+
     for(string line : data) 
     {
-        
         for(int i = 0; i < line.length() - 1; i += 2) 
         {
-
             int currentColumn = line.at(i) - '0';
             int nextColumn = line.at(i+2) - '0';
             
-            if(currentColumn == 0 || nextColumn == 0) 
-            {
-                res += 1;
-                cout << currentColumn << " " << nextColumn << "   " << res << endl;
-                continue;
-            }
-
             if(currentColumn != nextColumn) 
             {
-                res += 2;
-                cout << currentColumn << " " << nextColumn << "   " << res << endl;
+                resHor++;
                 continue;
             }
-            cout << currentColumn << " " << nextColumn << "   " << res << endl;
-
-
-            
-           
         }
+        
+        cout << "Horizontal sides: " << resHor << endl;
+
+        for(int i = 0; i < line.length(); i += 2) 
+        {
+            int currentColumn = line.at(i) - '0';
+            int prevColumn = -1;
+
+            if(i - 2 >= 0) prevColumn = line.at(i-2) - '0';
+            else 
+            {
+                resVer++;
+                continue;
+            }
+
+            if(currentColumn == 0) continue;
+
+            if(currentColumn != prevColumn) 
+            {
+                resVer++;
+                continue;
+            }
+            
+
+        }
+        
+        cout << "Vertical sides: " << resVer << endl;
     }
-    return res;
+
+    return resHor + resVer;
 }
 
 int countSides3D(vector<string> data)
 {
-    int res = 6;
-    for(int y = 0; y < data.size(); y++) 
-    {
-        string line = data.at(y);
+   int res = 0;
 
-        for(int x = 0; x < line.length(); x += 2)
-        {
-            int currentColumn = line.at(x) - '0';
-            /*int rightColumn = -1;
-            int leftColumn = -1;
-            int upColumn = -1;
-            int downColumn = -1;*/
-
-            vector<int> neighbours(4,-1); //clokwise - 0 - top, 1 - right, 2 - bottom, 3 - left
-
-            if(x + 2 < line.length()) neighbours.at(1) = line.at(x + 2) - '0';
-            if(x - 2 >= 0) neighbours.at(3) = line.at(x - 2) - '0';
-            if(y+1 < data.size()) 
-            {
-                string tmpLine = data.at(y+1);
-                if(x < tmpLine.length()) neighbours.at(2) = tmpLine.at(x) - '0';
-            } 
-            if(y-1 >= 0) 
-            {
-                string tmpLine = data.at(y-1);
-                if(x < tmpLine.length()) neighbours.at(0) = tmpLine.at(x) - '0';
-            } 
-            //cout << currentColumn << endl;
-            for(int neighbour : neighbours) 
-            {
-                //cout << neighbour << " ";
-                if(neighbour == -1) continue;
-                if(currentColumn == 0 || neighbour == 0) 
-                {
-                    res += 1;
-                    
-                    continue;
-                }
-
-                if(currentColumn != neighbour) 
-                {
-                    res += 2;
-                    continue;
-                }
-            }
-            //cout << endl;
-        }
-
-    }
-    return res;
+   return res;
 }
 
 int main() 
 {
 
-    vector<string> data = readDataFromFile("HeightMap1.txt");
+    vector<string> data = readDataFromFile("data.txt");
+
+    int res = countSides2D(data);
+
+    cout << "Number of sides: " << res << endl;
+
+    /*vector<string> data = readDataFromFile("HeightMap1.txt");
     cout << "HeightMap1.txt" << endl;
     for(string line : data) {
         cout << line << endl;
@@ -148,7 +122,7 @@ int main()
         cout << line << endl;
     }
     res = countSides3D(data);
-    cout << "Number of sides: " << res << endl;
+    cout << "Number of sides: " << res << endl;*/
 
     return 0;
 }
