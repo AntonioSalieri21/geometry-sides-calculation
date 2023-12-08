62841e7b157218d429a6ef4b983e08b555dec55e
AntonioSalieri21 <illia.antonov.st@vsb.cz>
2023-12-08 13:12:44 +0100

    3D side count algorithm implemented
    Implemented algorithm, described in readme and previous commit, but for 3D spaces.
    There are also needed little changes - I have written code for not only rectangle, but another voxel shapes too - but in task they are always in rectangle shape, so I need to rewrite code for this task. But at all code is fully functional.

--- main.cpp
+++ main.cpp
@@ -10,7 +10,12 @@ vector<string> readDataFromFile(string file)
     ifstream inputFile(file);
     if (inputFile.is_open()) {
         string line;
+        int i = 0;
         while (getline(inputFile, line)) {
+            if(i == 0) {
+                i++;
+                continue;
+            }
             res.push_back(line);
         }
         inputFile.close();
@@ -72,21 +77,106 @@ int countSides2D(vector<string> data)
 
 int countSides3D(vector<string> data)
 {
-   int res = 0;
+    int resX = 2; //left and right sides always exist
+    int resY = 1; //bottom side always exists
+    int resZ = 2; //front and back sides always exist
 
-   return res;
+   //calculating left to right sides
+   for(string line : data)
+   {
+         for(int i = 0; i < line.length() - 1; i += 2)
+         {
+              
+              int currentColumn = line.at(i) - '0';
+              if(i + 2 >= line.length()) break; //if we are at the last column, we don't need to check the next one (it doesn't exist)
+              int nextColumn = line.at(i+2) - '0';
+
+              if(currentColumn != nextColumn)
+              {
+                resX++;
+                continue;
+              }
+         }
+         
+   }
+    cout << "Left to right sides: " << resX << endl;
+
+    //calculating front to back sides
+    for(int i = 0; i < data.size() - 1; i++)
+    {
+        string currentLine = data.at(i);
+        string nextLine = data.at(i+1);
+
+        for(int j = 0; j < currentLine.length(); j += 2)
+        {
+            int currentColumn = currentLine.at(j) - '0';
+            if(j >= nextLine.length())
+            {
+                resZ++;
+                continue;
+            }
+
+            int nextColumn = nextLine.at(j) - '0';
+
+            if(currentColumn != nextColumn)
+            {
+                resZ++;
+                continue;
+            }
+        }
+        
+    }
+    cout << "Front to back sides: " << resZ << endl;
+
+    //calculating bottom to top sides
+    for(int i = 0; i < data.size(); i++)
+    {
+        string currentLine = data.at(i);
+        for(int j = 0; j < currentLine.length(); j += 2)
+        {
+            int currentColumn = currentLine.at(j) - '0';
+            if(currentColumn == 0) continue;
+
+            int leftColumn = 0;
+            int topColumn = 0;
+
+            if(j - 2 >= 0) leftColumn = currentLine.at(j-2) - '0';
+            if(i - 1 >= 0) 
+            {
+                if(j < data.at(i-1).length()) topColumn = data.at(i-1).at(j) - '0';
+            }
+
+            if(leftColumn != currentColumn && topColumn != currentColumn)
+            {
+                cout << "leftColumn: " << leftColumn << " " << "topColumn: " << topColumn << " " << "currentColumn: " << currentColumn << endl;
+                resY++;
+                continue;
+            }
+        }
+        
+    }
+    cout << "Bottom to top sides: " << resY << endl;
+    
+
+   return resX + resY + resZ;
 }
 
 int main() 
 {
 
-    vector<string> data = readDataFromFile("data.txt");
+    /*vector<string> data = readDataFromFile("data.txt");
 
     int res = countSides2D(data);
 
-    cout << "Number of sides: " << res << endl;
+    cout << "Number of sides: " << res << endl;*/
 
-    /*vector<string> data = readDataFromFile("HeightMap1.txt");
+    /*vector<string> data = readDataFromFile("HeightMap2.txt");
+
+    int res = countSides3D(data);
+
+    cout << "Number of sides: " << res << endl;*/
+
+    vector<string> data = readDataFromFile("HeightMap1.txt");
     cout << "HeightMap1.txt" << endl;
     for(string line : data) {
         cout << line << endl;
@@ -122,7 +212,7 @@ int main()
         cout << line << endl;
     }
     res = countSides3D(data);
-    cout << "Number of sides: " << res << endl;*/
+    cout << "Number of sides: " << res << endl;
 
     return 0;
 }
